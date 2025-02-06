85894014a903887417b978ee56cebd795c2cc0c6
MostakemHossain <mostakememon123@gmail.com>
2025-02-06 21:14:36 +0600

    cleark authentication added

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return ( 
+        <div>
+            only authorized users
+        </div>
+     );
+}
+ 
+export default Page;
--- src/modules/home/ui/components/home-navbar/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-navbar/home-sidebar/main-section.tsx
@@ -7,6 +7,8 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+
+import { useAuth, useClerk } from "@clerk/nextjs";
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 import Link from "next/link";
 
@@ -30,6 +32,8 @@ const items = [
 ];
 
 export const MainSection = () => {
+  const { isSignedIn } = useAuth();
+  const clerk = useClerk();
   return (
     <SidebarGroup>
       <SidebarGroupContent>
@@ -40,14 +44,18 @@ export const MainSection = () => {
                 tooltip={item.title}
                 asChild
                 isActive={false}
-                onClick={() => {}}
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                   return clerk.openSignIn();
+                  }
+                }}
               >
                 <Link href={item.url} className="flex items-center gap-4">
                   <item.icon />
                   <span className="text-sm">{item.title}</span>
                 </Link>
               </SidebarMenuButton>
-            
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
