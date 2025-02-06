b4eab15011ba34a1929cde15e5ddb77f4f544a1e
MostakemHossain <mostakememon123@gmail.com>
2025-02-06 18:11:12 +0600

    Home sidebar added

--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavbar } from "../components/home-navbar";
-
+import { HomeSidebar } from "../components/home-navbar/home-sidebar";
 
 interface HomeLayout {
   children: React.ReactNode;
@@ -11,8 +11,10 @@ export const HomeLayout = ({ children }: HomeLayout) => {
     <SidebarProvider>
       <div className="w-full">
         <HomeNavbar />
-        <div>{children}
-          
+        <div className="flex min-h-screen pt-[4rem]">
+          <HomeSidebar/>
+          <main className="flex-1 overflow-y-auto">
+            {children}</main>
         </div>
       </div>
     </SidebarProvider>
