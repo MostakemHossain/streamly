0b1c575b877415bf3c18ed4c7f0e962c6cf2fdce
MostakemHossain <mostakememon123@gmail.com>
2025-02-07 19:44:34 +0600

    trpc configuration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,20 @@
-import { trpc } from "@/trpc/server";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { PageClient } from "./client";
 
 export default async function Home() {
-  const data = await trpc.hello({
-    text: "Hello",
+  void trpc.hello.prefetch({
+    text: "Mostakem",
   });
 
   return (
-    <div>
-      I will load videos in future
-      {data?.greeting}
-    </div>
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
