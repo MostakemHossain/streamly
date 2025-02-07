e519cedd8b3b9af57a1f78c5a0377b2d8fb88ca4
MostakemHossain <mostakememon123@gmail.com>
2025-02-07 14:15:29 +0600

    trpc configuration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div>I will load videos in future</div>;
+import { trpc } from "@/trpc/server";
+
+export default async function Home() {
+  const data = await trpc.hello({
+    text: "Hello",
+  });
+
+  return (
+    <div>
+      I will load videos in future
+      {data?.greeting}
+    </div>
+  );
 }
